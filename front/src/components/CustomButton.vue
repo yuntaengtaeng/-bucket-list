a642ce5629e072c0522f53c3c1ee7e89e79be643
yuntaengtaeng <k76253218@gmail.com>
2023-01-21 20:57:13 +0900

    feat : CustomButton, CustomInput 컴포넌트 추가

---
+++ front/src/components/CustomButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "custom-button",
+});
+</script>
+
+<style scoped lang="scss">
+button {
+  color: white;
+  font-weight: 600;
+  font-size: 20px;
+  padding: 8px 24px;
+  border-radius: 4px;
+  background-color: #8c8cf8;
+  border: none;
+
+  &:disabled {
+    opacity: 0.8;
+  }
+}
+</style>
---
+++ front/src/components/CustomInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <input :type="type" :value="value" @input="onChange" />
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "custom-input",
+  props: {
+    type: String,
+    value: String,
+  },
+  emits: ["update:modelValue"],
+  methods: {
+    onChange(event: Event) {
+      const target = event.target as HTMLInputElement;
+      this.$emit("update:modelValue", target.value);
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+input {
+  font-size: 18px;
+  padding: 8px;
+  border-radius: 4px;
+  border-color: #e5e7eb;
+}
+</style>
