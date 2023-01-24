4df81ba279ab2c8e7d70cab70520f1f16b302a8d
yuntaengtaeng <k76253218@gmail.com>
2023-01-24 14:18:32 +0900

    feat : axios baseURL 설정 후 글로벌 변수에 할당

--- front/src/main.ts
+++ front/src/main.ts
@@ -2,5 +2,17 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
 
-createApp(App).use(store).use(router).mount("#app");
+// TODO : 폴더 분리하기
+declare module "vue" {
+  interface ComponentCustomProperties {
+    $axios: typeof axios;
+  }
+}
+
+const app = createApp(App);
+axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URI;
+app.config.globalProperties.$axios = axios;
+
+app.use(store).use(router).mount("#app");
