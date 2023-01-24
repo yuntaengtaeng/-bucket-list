d05a824e7528d8fad372a660c63ccfdbbd6fed8f
sinyeongseok <sinyeongseok99@gmail.com>
2023-01-24 20:04:40 +0900

    feat: 카테고리 목록 가져오기 API추가

--- back/src/api/api.js
+++ back/src/api/api.js
@@ -4,4 +4,13 @@ const app = express();
 const apiMain = require('./main/index');
 app.use('/main', apiMain);
 
+const apiCategory = require('./category/index');
+app.use('/category', apiCategory);
+
+// const apiAuth = require('./auth/index');
+// app.use('/auth', apiAuth);
+
+// const apiToken = require('./token/index');
+// app.use('/token', apiToken);
+
 module.exports = app;
---
+++ back/src/api/category/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const categoryModel = require('../../models/caregory');
+const cors = require('cors');
+const app = express();
+const MONGODB_URI = process.env.MONGODB_URI;
+app.use(cors());
+
+mongoose.set('strictQuery', false);
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGODB_URI, { dbName: 'bucket', useNewUrlParser: true });
+
+const formatCategoryList = (categoryList) => {
+  return categoryList.map((categoryInfo) => {
+    return {
+      id: categoryInfo._id,
+      name: categoryInfo.name,
+      icon: categoryInfo.icon,
+    };
+  });
+};
+
+app.get('/', async (req, res) => {
+  try {
+    const oldCategoryList = await categoryModel.find();
+    const categoryList = formatCategoryList(oldCategoryList);
+
+    res.status(200).json({ categoryList });
+  } catch (err) {
+    res.status(500).json({ message: '서버요청 실패' });
+  }
+});
+
+module.exports = app;
