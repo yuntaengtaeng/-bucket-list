f432e17335ec3467abafc8bf488426908fe7da62
yuntaengtaeng <k76253218@gmail.com>
2023-01-25 22:51:39 +0900

    feat : 메인에서 사용할 bucket 작성으로 이동 시키는 컴포넌트 추가

---
+++ front/src/components/MoveEdit.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrap box" @click="moveEdit">
+    <div class="box inner">
+      버킷리스트를 작성해보세요!
+      <i>📝</i>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "custom-input",
+  methods: {
+    moveEdit() {
+      this.$router.push("/edit");
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.box {
+  padding: 16px;
+  border-radius: 4px;
+  border: 1px solid #e5e7eb;
+}
+
+.inner {
+  width: 80%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
