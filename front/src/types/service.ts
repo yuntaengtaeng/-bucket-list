0d7d86ab3e6ef81f82a6f44003869b65c127b388
yuntaengtaeng <k76253218@gmail.com>
2023-01-26 21:58:03 +0900

    feat : types/service 추가
    - response error data type(api 요청으로 받는 에러 객체)
    - UserInfo(vuex로 관리할 전역 상태 타입)
    - CategorList(Home에서 보여주는 카테고리 data에 대한 type)

---
+++ front/src/types/service.ts
@@ -0,0 +1,14 @@
+export interface ErrorData {
+  message: string;
+}
+
+export interface UserInfo {
+  nickname: string;
+  accessToken: string;
+}
+
+export interface CategoryList {
+  id: string;
+  name: string;
+  icon: string;
+}
