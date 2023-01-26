7170cff615a0b89d5c45bb093b6a11777be1de40
yuntaengtaeng <k76253218@gmail.com>
2023-01-26 22:00:05 +0900

    feat : vuex를 사용한 전역 상태관리 추가
    - nickname, accessToken 관리
    - get 로그인 상태, 닉네임, 엑세스 토큰
    - set 닉네임 엑세스 토큰, 초기화, 엑세스 토큰 세팅

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,13 +1,24 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import { store } from "./store";
+import { Store } from "vuex";
 import axios from "axios";
+import { UserInfo } from "./types/service";
 
 // TODO : 폴더 분리하기
 declare module "vue" {
   interface ComponentCustomProperties {
     $axios: typeof axios;
+  }
+}
+
+declare module "@vue/runtime-core" {
+  // declare your own store states
+
+  // provide typings for `this.$store`
+  interface ComponentCustomProperties {
+    $store: Store<UserInfo>;
   }
 }
 
--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -1,9 +1,38 @@
-import { createStore } from "vuex";
+// store.ts
+import { InjectionKey } from "vue";
+import { createStore, Store } from "vuex";
+import { UserInfo } from "@/types/service";
 
-export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+// define injection key
+export const key: InjectionKey<Store<UserInfo>> = Symbol();
+
+export const store = createStore<UserInfo>({
+  state: {
+    nickname: "",
+    accessToken: "",
+  },
+  getters: {
+    getIsLoggedIn(state) {
+      return !!state.nickname;
+    },
+    getNickname(state) {
+      return state.nickname;
+    },
+    getAccessToken(state) {
+      return state.accessToken;
+    },
+  },
+  mutations: {
+    setData(state, value) {
+      state.nickname = value.nickname;
+      state.accessToken = value.accessToken;
+    },
+    initData(state) {
+      state.nickname = "";
+      state.accessToken = "";
+    },
+    setAccessToken(state, accessToken) {
+      state.accessToken = accessToken;
+    },
+  },
 });
