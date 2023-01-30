2094612fcd70c2cb746747b79f5d9980e322d5cd
yuntaengtaeng <k76253218@gmail.com>
2023-01-30 22:26:47 +0900

    feat : axios interceptors를 사용하여 요청과 응답 시 추가 행동 작성
    - 요청 시 accessToken이 존재한다면 header에 담아서 요청
    - 응답 시 state가 419이면서 code가 expired라면 refreshToken을 재발급 받아서 요청

--- front/src/main.ts
+++ front/src/main.ts
@@ -24,6 +24,44 @@ declare module "@vue/runtime-core" {
 
 const app = createApp(App);
 axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URI;
+axios.interceptors.request.use((request) => {
+  const accessToken = store.getters.getAccessToken;
+
+  if (accessToken) {
+    request.headers.set(
+      "Authorization",
+      `${process.env.VUE_APP_ACCESS_TOKEN_KEY} ${accessToken}`
+    );
+  }
+  return request;
+});
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const {
+      config,
+      response: { status },
+    } = error;
+
+    if (status === 419) {
+      if (error.response.data.code === "expired") {
+        const originalRequest = config;
+
+        const {
+          data: { data },
+        } = await axios.post("/refresh-token");
+        store.commit("setAccessToken", data.accessToken);
+
+        return axios(originalRequest);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 app.config.globalProperties.$axios = axios;
 
 app.use(store).use(router).mount("#app");
