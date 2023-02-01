3ec289bfa4d88c4be5e6977775c02e8005a813d8
sinyeongseok <sinyeongseok99@gmail.com>
2023-02-01 23:03:26 +0900

    .

--- back/src/api/main/index.js
+++ back/src/api/main/index.js
@@ -15,14 +15,33 @@ mongoose.Promise = global.Promise;
 mongoose.connect(MONGODB_URI, { dbName: "bucket", useNewUrlParser: true });
 
 const formatBucketlist = (bucketlist) => {
-  return bucketlist.map((cur) => {
-    return {
-      id: cur._id,
-      categoryID: cur.category_id,
-      title: cur.title,
-      context: cur.context,
-    };
-  });
+  const { newBucketlist, checked } = bucketlist.reduce(
+    (res, cur) => {
+      if (cur.is_checked) {
+        res.checked += 1;
+      }
+
+      res.newBucketlist.push({
+        id: cur._id,
+        categoryID: cur.category_id,
+        title: cur.title,
+        context: cur.context,
+        isChecked: cur.is_checked,
+      });
+
+      return res;
+    },
+    { newBucketlist: [], checked: 0 }
+  );
+
+  return {
+    bucketlist: newBucketlist,
+    count: {
+      total: newBucketlist.length,
+      notChecked: newBucketlist.length - checked,
+      checked,
+    },
+  };
 };
 
 app.post("/bucklist", verifyAccessToken, async (req, res) => {
@@ -61,29 +80,136 @@ app.get("/bucklist/:categoryID", verifyAccessToken, async (req, res) => {
   };
 
   try {
-    const oldBucketlist = await bucketlistModel.find(searchOption);
-    const bucketlist = formatBucketlist(oldBucketlist);
+    const bucketlist = await bucketlistModel.find(searchOption);
+    const result = formatBucketlist(bucketlist);
 
-    res.status(200).json({ bucketlist });
+    res.status(200).json({ data: result });
   } catch (err) {
-    res.status(500).json({ message: "서버요청 실패" });
+    res.status(500).json({ message: "서버요청 실패", test: err.message });
   }
 });
 
-app.delete("/bucklist/:buckletistID", verifyAccessToken, async (req, res) => {
-  const buckletistID = req.params.buckletistID;
-  const id = res.locals.id;
+const formatBucketlistDetail = (bucketlistInfo) => {
+  return {
+    id: bucketlistInfo._id,
+    categoryID: bucketlistInfo.category_id,
+    title: bucketlistInfo.title,
+    context: bucketlistInfo.context,
+    isChecked: bucketlistInfo.is_checked,
+  };
+};
 
-  try {
-    await bucketlistModel.deleteOne({
-      _id: buckletistID,
-      user_id: id,
-    });
+app.get(
+  "/bucklist/detail/:bucketlistID",
+  verifyAccessToken,
+  async (req, res) => {
+    const bucketlistID = req.params.bucketlistID;
+    const id = res.locals.id;
 
-    res.status(200).json({ deleted: true });
-  } catch (err) {
-    res.status(500).json({ message: "서버요청 실패" });
+    try {
+      const bucketlistInfo = await bucketlistModel.findOne({
+        _id: bucketlistID,
+        user_id: id,
+      });
+      const result = formatBucketlistDetail(bucketlistInfo);
+
+      res.status(200).json({ bucklistInfo: result });
+    } catch (err) {
+      res.status(500).json({ message: "서버요청 실패", test: err.message });
+    }
   }
-});
+);
+
+app.delete(
+  "/bucklist/deleted/:buckletistID",
+  verifyAccessToken,
+  async (req, res) => {
+    const buckletistID = req.params.buckletistID;
+    const id = res.locals.id;
+
+    try {
+      await bucketlistModel.deleteOne({
+        _id: buckletistID,
+        user_id: id,
+      });
+
+      const bucketlist = await bucketlistModel.find({ user_id: id });
+      const result = formatBucketlist(bucketlist);
+
+      res.status(200).json({ data: result });
+    } catch (err) {
+      res.status(500).json({ message: "서버요청 실패" });
+    }
+  }
+);
+
+app.patch(
+  "/bucklist/patch/:buckletistID",
+  verifyAccessToken,
+  async (req, res) => {
+    const buckletistID = req.params.buckletistID;
+    const body = req.body;
+    const id = res.locals.id;
+
+    const patchOption = {
+      ...(!!body.categoryID && {
+        category_id: body.categoryID,
+      }),
+      ...(!!body.title && {
+        title: body.title,
+      }),
+      ...(!!body.context && {
+        context: body.context,
+      }),
+    };
+
+    try {
+      await bucketlistModel.updateOne(
+        {
+          _id: buckletistID,
+          user_id: id,
+        },
+        patchOption
+      );
+
+      res.status(200).json({ patched: true });
+    } catch (err) {
+      res.status(500).json({ message: "서버요청 실패" });
+    }
+  }
+);
+
+app.patch(
+  "/bucklist/checked/:buckletistID",
+  verifyAccessToken,
+  async (req, res) => {
+    const buckletistID = req.params.buckletistID;
+    const id = res.locals.id;
+
+    try {
+      const bucketlistInfo = await bucketlistModel.findOne({
+        _id: buckletistID,
+        user_id: id,
+      });
+
+      await bucketlistModel.updateOne(
+        {
+          _id: buckletistID,
+          user_id: id,
+        },
+        {
+          is_checked: !bucketlistInfo.is_checked,
+        }
+      );
+
+      const bucketlist = await bucketlistModel.find({ user_id: id });
+      const result = formatBucketlist(bucketlist);
+
+      res.status(200).json({ data: result });
+    } catch (err) {
+      res.status(500).json({ message: "서버요청 실패" });
+    }
+  }
+);
 
 module.exports = app;
